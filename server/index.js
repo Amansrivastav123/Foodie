5a8bcb71eb918321b466df2dd52f5e805ee8834f
JAYANT JAIN <56956286+jayant26@users.noreply.github.com>
2021-10-15 15:06:00 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -29,45 +29,6 @@ app.post('/register',(req,res)=>{
     
 })
 
-app.post('/requestdriver',(req,res)=>{
-
-    const driver_name= req.body.d_name
-    const driver_mobile= req.body.d_mobile
-    const driver_city= req.body.d_city
-    const driver_password= req.body.d_password
-        if(driver_name&&driver_mobile&&driver_city&&driver_password)
-        {
-            db.query("INSERT INTO driverrequest(driver_name,driver_mobile,driver_city,driver_password) VALUES (?,?,?,?)",[driver_name,driver_mobile,driver_city,driver_password],(err,result)=>{
-                console.log(err);
-            })
-        }
-    
-})
-
-
-
-app.post('/login/foodie',(req,res)=>{
-
-    
-    const foodie_mobile= req.body.username
-    const foodie_password= req.body.password
-
-    db.query("SELECT * FROM foodie WHERE foodie_mobile= ? AND foodie_password = ?",[foodie_mobile,foodie_password],(err,result)=>{
-       if(err)
-       {
-            res.send(err);
-       }
-       if(result.length>0)
-        {
-            res.send(result);
-        }else{
-            res.send({message:"Invalid mobile and password combination"})
-        }
-       
-        
-    })
-})
-
 
 app.listen(3001,()=>{
     console.log("running on port 3001")
